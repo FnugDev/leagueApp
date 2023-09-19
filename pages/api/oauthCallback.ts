601ffb1c0abbaf22c3f61da6e81953658b85bd21
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-19 02:34:30 +0200

    Riot oAuth added

--- pages/api/oauthCallback.ts
+++ pages/api/oauthCallback.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
-import querystring from 'querystring'; 
-import { v4 as uuidv4 } from 'uuid';
-import request from 'request';
-import jwt from 'jsonwebtoken';
+import querystring from 'querystring';
+import { Buffer } from 'buffer';
 
 // Environment Variables
 const clientId = process.env.RIOT_CLIENT_ID;
@@ -16,21 +14,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(500).send('Environment variables are not set.');
     }
 
-    const uniqueId = uuidv4();
-    const payload = {
-      iss: clientId,
-      sub: clientId,
-      aud: 'https://auth.riotgames.com/token',
-      jti: uniqueId,
-      exp: Math.floor(Date.now() / 1000) + 60,
-    };
+    // Generate the base64 encoded Authorization header
+    const authHeader = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
 
-    const signedJwt = jwt.sign(payload, clientSecret, { algorithm: 'HS256' });
-    
     const code = req.query.code as string;
     const tokenData = querystring.stringify({
-      client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-      client_assertion: signedJwt,
       grant_type: "authorization_code",
       code,
       redirect_uri: redirectUri,
@@ -38,16 +26,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     console.log('Token Data:', tokenData);
 
-
+    // Make the POST request to get the access token
     const tokenResponse = await axios.post('https://auth.riotgames.com/token', tokenData, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${authHeader}`
       },
     });
 
     const accessToken = tokenResponse.data.access_token;
 
     // The rest of your code...
+
   } catch (error) {
     console.error('Error: ', error.response?.data || error.message);
     res.status(500).json({ error: error.response?.data || error.message });
