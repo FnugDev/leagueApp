263386d94c50806aba4cdb2452af58977358e48e
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-19 12:01:36 +0200

    Riot oAuth added

---
+++ pages/api/oauthCallback.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+import querystring from 'querystring'; 
+import { v4 as uuidv4 } from 'uuid';
+import request from 'request';
+import jwt from 'jsonwebtoken';
+
+// Environment Variables
+const clientId = process.env.RIOT_CLIENT_ID;
+const clientSecret = process.env.RIOT_CLIENT_SECRET;
+const redirectUri = process.env.RIOT_REDIRECT_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (!clientId || !clientSecret || !redirectUri) {
+      return res.status(500).send('Environment variables are not set.');
+    }
+
+    const uniqueId = uuidv4();
+    const payload = {
+      iss: clientId,
+      sub: clientId,
+      aud: 'https://auth.riotgames.com/token',
+      jti: uniqueId,
+      exp: Math.floor(Date.now() / 1000) + 60,
+    };
+
+    const signedJwt = jwt.sign(payload, clientSecret, { algorithm: 'HS256' });
+    
+    const code = req.query.code as string;
+    const tokenData = {
+      client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+      client_assertion: signedJwt,
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: redirectUri,
+    };
+    
+
+    const tokenResponse = await axios.post('https://auth.riotgames.com/token', tokenData);
+
+    const accessToken = tokenResponse.data.access_token;
+
+    // The rest of your code...
+  } catch (error) {
+    console.error('Error: ', error.response?.data || error.message);
+    res.status(500).json({ error: error.response?.data || error.message });
+  }
+};
+
+export default handler;
