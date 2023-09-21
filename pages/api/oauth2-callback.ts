c976ce81571e880665a400aaeb767b291a6cb225
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-22 00:30:04 +0200

    Riot oAuth added

--- pages/api/oauth2-callback.ts
+++ pages/api/oauth2-callback.ts
@@ -1,17 +1,12 @@
 import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { v4 as uuidv4 } from 'uuid';
-import jwt from 'jsonwebtoken';
+import Cookies from 'js-cookie';
+
 
 const clientId = process.env.RIOT_CLIENT_ID;
 const clientSecret = process.env.RIOT_CLIENT_SECRET;
 const redirectUri = process.env.RIOT_REDIRECT_URI_CALLBACK;
 const tokenUrl = "https://auth.riotgames.com/token"; 
-
-// Import other required modules
-// ...
-
-let refreshToken: string;  // Store this securely
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -21,20 +16,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const accessCode = req.query.code as string;
     const auth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-    
+
     const data = new URLSearchParams();
-    if (accessCode) {
-      // For initial authentication
-      data.append('grant_type', 'authorization_code');
-      data.append('code', accessCode);
-      data.append('redirect_uri', redirectUri);
-    } else if (refreshToken) {
-      // For token refresh
-      data.append('grant_type', 'refresh_token');
-      data.append('refresh_token', refreshToken);
-    } else {
-      return res.status(400).send('No code or refresh token provided.');
-    }
+    data.append('grant_type', 'authorization_code');
+    data.append('code', accessCode);
+    data.append('redirect_uri', redirectUri);
+    
 
     const fetchOptions = {
       method: 'POST',
@@ -47,10 +34,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const fetchResponse = await fetch(tokenUrl, fetchOptions);
     const responseData = await fetchResponse.json();
 
+    Cookies.set('accessToken', responseData.access_token, { expires: 1 });
+
     if (fetchResponse.ok) {
-      if (responseData.refresh_token) {
-        refreshToken = responseData.refresh_token;  // Store this securely
-      }
       res.json({ success: true, data: responseData });
     } else {
       res.status(fetchResponse.status).json({ success: false, error: responseData });
