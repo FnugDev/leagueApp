cad2569e7ce306fc2da401f29452a7566939e3ed
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-21 17:39:43 +0200

    Riot oAuth added

--- pages/api/oauth2-callback.ts
+++ pages/api/oauth2-callback.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
+const request = require('request');
+
+
 
 const clientId = process.env.RIOT_CLIENT_ID;
 const clientSecret = process.env.RIOT_CLIENT_SECRET;
@@ -14,22 +17,39 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(500).send('Environment variables are not set.');
     }
 
-    const accessCode = req.query.code as string;
+    const accessCode = req.query.code;
 
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    };
+    // const headers = {
+    //   'Content-Type': 'application/x-www-form-urlencoded'
+    // };
 
-    const auth = {
-      auth: {
-        username: clientId,
-        password: clientSecret,
-      },
-    };
+    // const auth = {
+    //   auth: {
+    //     username: clientId,
+    //     password: clientSecret,
+    //   },
+    // };
 
-    const data = `grant_type=authorization_code&code=${encodeURIComponent(accessCode)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+    request.post({
+      url: tokenUrl,
+      auth: { // sets "Authorization: Basic ..." header
+          user: clientId,
+          pass: clientSecret
+       },
+       form: { // post information as x-www-form-urlencoded
+           grant_type: "authorization_code",
+           code: accessCode, // accessCode should be url decoded before being set here
+           redirect_uri: redirectUri
+       }
+    }, function (error, response, body) {
+       // do something with the response?
+    });
 
-    const response = await axios.post(tokenUrl, data, { ...auth, headers });
+
+
+    // const data = `grant_type=authorization_code&code=${encodeURIComponent(accessCode)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+    // const response = await axios.post(tokenUrl, data, { ...auth, headers });
 
   } catch (error) {
     console.error('Error: ', error.response?.data || error.message);
