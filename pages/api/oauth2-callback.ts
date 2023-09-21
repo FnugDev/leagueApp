79248be05aa751e3204b1a8e112c9ab7e7b0d669
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-22 00:21:41 +0200

    Riot oAuth added

--- pages/api/oauth2-callback.ts
+++ pages/api/oauth2-callback.ts
@@ -8,6 +8,11 @@ const clientSecret = process.env.RIOT_CLIENT_SECRET;
 const redirectUri = process.env.RIOT_REDIRECT_URI_CALLBACK;
 const tokenUrl = "https://auth.riotgames.com/token"; 
 
+// Import other required modules
+// ...
+
+let refreshToken: string;  // Store this securely
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (!clientId || !clientSecret || !redirectUri) {
@@ -16,12 +21,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const accessCode = req.query.code as string;
     const auth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-
-    const data = new URLSearchParams();
-    data.append('grant_type', 'authorization_code');
-    data.append('code', accessCode);
-    data.append('redirect_uri', redirectUri);
     
+    const data = new URLSearchParams();
+    if (accessCode) {
+      // For initial authentication
+      data.append('grant_type', 'authorization_code');
+      data.append('code', accessCode);
+      data.append('redirect_uri', redirectUri);
+    } else if (refreshToken) {
+      // For token refresh
+      data.append('grant_type', 'refresh_token');
+      data.append('refresh_token', refreshToken);
+    } else {
+      return res.status(400).send('No code or refresh token provided.');
+    }
 
     const fetchOptions = {
       method: 'POST',
@@ -35,6 +48,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const responseData = await fetchResponse.json();
 
     if (fetchResponse.ok) {
+      if (responseData.refresh_token) {
+        refreshToken = responseData.refresh_token;  // Store this securely
+      }
       res.json({ success: true, data: responseData });
     } else {
       res.status(fetchResponse.status).json({ success: false, error: responseData });
