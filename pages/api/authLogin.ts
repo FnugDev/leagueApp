2aad87d72123cd51fe1af5a65fec50ecb5502cb0
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-22 01:14:21 +0200

    Riot oAuth added

--- pages/api/authLogin.ts
+++ pages/api/authLogin.ts
@@ -1,31 +1,17 @@
-// pages/api/login.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 
-interface QueryParams {
-  prevUrl?: string;
-}
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { prevUrl }: QueryParams = req.query;
-  
-  // Use environment variables
   const clientId = process.env.RIOT_CLIENT_ID;
   const redirectUri = process.env.RIOT_REDIRECT_URI_CALLBACK;
 
-  // Check for missing environment variables
   if (!clientId || !redirectUri) {
     res.status(500).send("Environment variables are not set.");
     return;
   }
 
-  // Optional: You can use the prevUrl for some purpose if needed
-  if (prevUrl) {
-    // Do something with prevUrl if needed
-  }
-
   try {
     res.redirect(
-      `https://auth.riotgames.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid+offline_access`
+      `https://auth.riotgames.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid+offline_access+cpid`
     );
   } catch (error) {
     console.error("Error redirecting: ", error);
