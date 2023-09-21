2606a73cb223a70372a4285c8ab40af41a67b384
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-21 02:02:59 +0200

    Riot oAuth added

--- pages/api/oauth2-callback.ts
+++ pages/api/oauth2-callback.ts
@@ -2,21 +2,16 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
-import fs from 'fs';
-import path from 'path';
 
 const clientId = process.env.RIOT_CLIENT_ID;
+const clientSecret = process.env.RIOT_CLIENT_SECRET;
 const redirectUri = process.env.RIOT_REDIRECT_URI;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (!clientId || !redirectUri) {
+    if (!clientId || !clientSecret || !redirectUri) {
       return res.status(500).send('Environment variables are not set.');
     }
-
-    // Load the private key from the same directory as this file
-    const privateKey = fs.readFileSync(path.resolve(process.cwd(), 'public/private.pem'), 'utf8');
-
 
     const uniqueId = uuidv4();
     const payload = {
@@ -24,12 +19,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       sub: clientId,
       aud: 'https://auth.riotgames.com/token',
       jti: uniqueId,
-      exp: Math.floor(Date.now() / 1000) + 6000,
+      exp: Math.floor(Date.now() / 1000) + 60000000,
     };
 
-    // Sign the JWT with the private key
-    const signedJwt = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-
+    const signedJwt = jwt.sign(payload, clientSecret, { algorithm: 'HS384' });
+    
     const code = req.query.code as string;
     const tokenData = {
       client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
@@ -38,15 +32,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       code,
       redirect_uri: redirectUri,
     };
-
+    
     const tokenResponse = await axios.post('https://auth.riotgames.com/token', tokenData, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
     });
-
-    res.status(200).json(tokenResponse.data);
-
   } catch (error) {
     console.error('Error: ', error.response?.data || error.message);
     res.status(500).json({ error: error.response?.data || error.message });
