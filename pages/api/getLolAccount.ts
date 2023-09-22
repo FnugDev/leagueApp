5f1053d6ee699ba7310346a15cadaca567919b08
Fnug <62522314+FnugDev@users.noreply.github.com>
2023-09-22 02:03:35 +0200

    Riot oAuth added

--- pages/api/getLolAccount.ts
+++ pages/api/getLolAccount.ts
@@ -12,30 +12,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const cpidResponse = await axios.get('https://auth.riotgames.com/userinfo', {
       headers: { 'Authorization': `Bearer ${accessToken}` }
     });
-
-    // Log only the necessary data
-    console.log(cpidResponse.data);
-
-    // Validation
-    if (!cpidResponse.data || !cpidResponse.data.cpid) {
-      return res.status(500).json({ error: 'CPID not found' });
-    }
+  
+    console.log(cpidResponse.data)
 
     const cpid = cpidResponse.data.cpid;
-
-    const accountResponse = await axios.get(`https://${cpid}.api.riotgames.com/lol/summoner/v4/summoners/me`, {
+  
+    const summonerResponse = await axios.get(`https://${cpid}.api.riotgames.com/lol/summoner/v4/summoners/me`, {
+      headers: { 'Authorization': `Bearer ${accessToken}` }
+    });
+    
+    const accountResponse = await axios.get(`https://${cpid}.api.riotgames.com/lol/account/v1/accounts/me`, {
       headers: { 'Authorization': `Bearer ${accessToken}` }
     });
 
-    return res.json({ cpid, account: accountResponse.data });
+    return res.json({ cpid, summoner: summonerResponse, account: accountResponse.data });
   } catch (error) {
-    // Enhanced error handling
-    if (error.response) {
-      console.log("Data:", error.response.data);
-      console.log("Status:", error.response.status);
-      console.log("Headers:", error.response.headers);
-    }
-
     return res.status(500).json({ error: 'Failed to fetch account info' });
   }
 };
